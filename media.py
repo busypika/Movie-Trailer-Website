08ea2d59a6c80591508f5b2a7a1709d57cade04a
busypika <38233939+busypika@users.noreply.github.com>
2018-04-23 02:50:02 +0800

    Update media.py

--- media.py
+++ media.py
@@ -3,10 +3,8 @@ import webbrowser
 class Movie():
     """ This class provides a way to store movie related informations """
     
-    def __init__(self, movie_tite, movie_storyline,
-                 poster_image, trailer_youtube):
+    def __init__(self, movie_tite, poster_image, trailer_youtube):
         self.title = movie_tite
-        self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
         
